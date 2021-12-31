12fdc59e660244625d6c736099772046fad632d5
DomC93 <dcostanz93@gmail.com>
2021-12-31 01:21:11 -0500

    JS Hamburger Menu Added
    Hamburger Menu was added to  mobile navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 // JavaScript Document
 
-console.log("Welcome to My Portfolio Site!")
 
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
